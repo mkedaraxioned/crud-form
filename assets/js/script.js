d856342bd5e0d8d65e92bb22b7210473d5fc6c56
mkedaraxioned <kedarm@prdxn.com>
2021-03-30 16:15:32 +0530

    removal of unwanted code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 
 */
 let firstname,lastname,address,male,female,terms;
-const form = document.getElementById('personinfo');
+const form = document.getElementsByTagName("form")[0];
 form.addEventListener('submit', validateForm);
-const resetbtn=document.getElementById('resetbtn');
+const resetbtn=document.getElementsByClassName('resetbtn')[0];
 let form_ip=document.getElementsByClassName('form-ip');
 
 resetbtn.addEventListener('click',() =>{
@@ -12,40 +12,21 @@ resetbtn.addEventListener('click',() =>{
     selected_ul=null;
 });
 let persons= {};
-let person = {};
 let inputs=['firstname','lastname','male','female','address','terms'];
-let counter = 0;
 let selected_ul=null;
 function validateForm(event) {
     event.preventDefault();
     if(selected_ul!=null) {
-        // alert("selected row");
         rowEditSubmit();
     }
-    // console.log(inputs);
-    // for (let i = 0; i < 3; i++) {
-    //     persons.inputs[i] = form_ip[i].value; 
-    // }
 
     persons.firstname=form_ip[0].value;
     persons.lastname=form_ip[1].value;
     persons.address=form_ip[4].value;
-    // console.log(persons.firstname);
-    // for (let i = 3; i < inputs.length; i++) {
-    //     persons.inputs[i] = form_ip[i].checked;        
-    // }
-
     persons.male=form_ip[2].checked;
     persons.female=form_ip[3].checked;
     persons.terms=form_ip[5].checked;
 
-
-    // firstname = document.getElementById('firstname').value;
-    // lastname = document.getElementById('lastname').value;
-    // address = document.getElementById('address').value;
-    // male = document.getElementById('male').checked;
-    // female = document.getElementById('female').checked;
-    // terms=document.getElementById('terms').checked;
     let formFlag=0;
 
     for(let i = 0 ; i < inputs.length; i++)
@@ -58,44 +39,12 @@ function validateForm(event) {
             formFlag=1; 
         }
     }
-
-    // if(firstname=="")
-    //   {
-    //     alert("Please enter Firstname");
-    //     formFlag=1;
-    //   }
-    //   if(lastname=="")
-    //   {
-    //     alert("Please enter Lastname");
-    //     formFlag=1;
-    //   }
-    //   if(address=="")
-    //   {
-    //     alert("Please enter Address");
-    //     formFlag=1;
-    //   }
-    //   if(!male)
-    //   {
-    //       if(!female)
-    //       {
-    //         alert("Please Choose gender");
-    //         formFlag=1;
-    //       }
-
-    //   }
-
-
      
           if((!persons.male)&&(!persons.female))
           {
               alert("Please Choose gender");
               formFlag=1; 
-          }
-      
-    //   if(!terms){
-    //     alert("Please Accept terms and Conditions by selecting checkbox");
-    //     formFlag=1;
-    //   }
+          }      
 
       if(!persons.terms){
         alert("Please Accept terms and Conditions by selecting checkbox");
@@ -113,55 +62,21 @@ function validateForm(event) {
         {
             gender="Female";
         }
-        // person.firstname=firstname;
-        // person.lastname=lastname;
-        // person.gender=gender;
-        // person.address=address;
-        // console.log(persons[inputs[3]]);
         if(selected_ul==null){
-            // persons[counter].id=counter;
             insertData(persons.firstname,persons.lastname,gender,persons.address);
-            counter++;
         }
 
       }
 
   }
 
-function insertData(firstname,lastname,gender,address){
-    // let tbody= document.getElementById('tbody');
-    // let row=tbody.insertRow(0);
-    // let tableFirstname = row.insertCell(0);
-    // let tableLastname = row.insertCell(1);
-    // let tableGender=row.insertCell(2);
-    // let tableAddress=row.insertCell(3);
-    // let tableEdit=row.insertCell(4);
-    // let tableDelete=row.insertCell(5);
-    // tableFirstname.innerHTML =firstname;
-    // tableLastname.innerHTML=lastname;
-    // tableGender.innerHTML=gender;
-    // tableAddress.innerHTML=address;
-    // tableEdit.innerHTML='<button onClick="rowEdit(this)">Edit</button>';
-    // tableDelete.innerHTML='<button onClick="rowDelete(this)">Delete</button>';
-
-
+function insertData(firstname,lastname,gender,address) {
     let div_to_add=document.getElementsByClassName('personlist')[0];
     let ul_main= document.createElement('ul');
-    // for(let i=0;i<4;i++){
-        // let li[i]= document.createElement('li');
-    //     li[i].innerHTML="<span>"+inputs[i]+"</span>";
-    // }
-
     let li=[];
-   for(let i=0;i<6;i++){
+   for(let i=0;i<6;i++) {
          li[i]= document.createElement('li');
     }
-    // let li0= document.createElement('li');
-    // let li1= document.createElement('li');
-    // let li2= document.createElement('li');
-    // let li3= document.createElement('li');
-    // let li4= document.createElement('li');
-    // let li5= document.createElement('li');
 
     li[0].innerHTML="<span>"+firstname+"</span>";
     li[1].innerHTML="<span>"+lastname+"</span>";
@@ -181,19 +96,6 @@ function insertData(firstname,lastname,gender,address){
 }
 
 function clearForm() {
-    // document.getElementById('firstname').value="";
-    // document.getElementById('lastname').value="";
-    // document.getElementById('address').value="";
-    // document.getElementById('male').checked="";
-    // document.getElementById('female').checked="";
-    // document.getElementById('terms').checked="";
-    // for (let i = 0; i < 3; i++) {
-    //     form_ip[i].value="";        
-    // }
-    // for (let i = 3; i < inputs.length; i++) {
-    //     form_ip[i].checked=false;
-    //     // previously marked ""        
-    // }
     for(let i = 0 ; i < inputs.length; i++)
     {   
         if(i===2||i===3||i===5){
@@ -203,7 +105,6 @@ function clearForm() {
         form_ip[i].value="";        
     }
     selected_ul = null;
-
 }
 
 function rowEdit(sel_span) {
@@ -226,9 +127,6 @@ function rowEditSubmit() {
     selected_ul.childNodes[0].childNodes[0].innerHTML=form_ip[0].value;
     selected_ul.childNodes[1].childNodes[0].innerHTML=form_ip[1].value;
     selected_ul.childNodes[3].childNodes[0].innerHTML=form_ip[4].value;    
-
-    // selected_ul.cells[0].innerHTML = document.getElementById("firstname").value ;
-    // selected_ul.cells[1].innerHTML = document.getElementById("lastname").value;
     if(form_ip[2].checked){
         selected_ul.childNodes[2].childNodes[0].innerHTML="Male";
     }
